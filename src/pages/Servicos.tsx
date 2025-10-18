afc12cc97110d6ee64043efa4856b03a61f27eef
tiagorezeck <tiagorezeck@hotmail.com>
2025-10-18 01:30:13 -0300

    Update Servicos.tsx

--- src/pages/Servicos.tsx
+++ src/pages/Servicos.tsx
@@ -76,7 +76,7 @@ const Servicos = () => {
             </div>
 
             {/* Pilares Grid */}
-            <div className="grid md:grid-cols-1 lg:grid-cols-7 gap-3 mb-6">
+            <div className="grid md:grid-cols-1 lg:grid-cols-7 gap-3 mb-2">
               {[{
               letter: 'C',
               icon: <Heart className="h-6 w-6" />,
