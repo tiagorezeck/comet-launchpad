18967351fba18c5e6cdf1f5e3dacef8ccf2e2203
tiagorezeck <tiagorezeck@hotmail.com>
2025-10-18 01:29:29 -0300

    Update Servicos.tsx

--- src/pages/Servicos.tsx
+++ src/pages/Servicos.tsx
@@ -217,7 +217,7 @@ const Servicos = () => {
               </p>
             </div>
 
-            <div className="grid md:grid-cols-1 lg:grid-cols-6 gap-2">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
               {[{
               step: '01',
               title: 'Contato Inicial',
