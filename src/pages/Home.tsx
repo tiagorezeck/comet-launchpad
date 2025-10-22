f20a15e555d834a5443395ec12f0a730817f69e3
tiagorezeck <tiagorezeck@hotmail.com>
2025-10-22 18:30:04 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import ClientCarousel from '@/components/ClientCarousel';
 import Testimonials from '@/components/Testimonials';
 import { Link } from 'react-router-dom';
 import { Rocket, Target, Users, Calendar, MessageCircle, Clock } from 'lucide-react';
-import heroBusiness from '@/assets/hero-business.jpg';
+import heroBusiness from '@/assets/hero-business.png';
 const Home = () => {
   return <div className="min-h-screen">
       {/* Hero Section */}
