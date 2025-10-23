bd2faa23548ee28805bc88e6e485d6e2dce92053
tiagorezeck <tiagorezeck@hotmail.com>
2025-10-23 00:43:08 -0300

    Update Vlog.tsx

--- src/pages/Vlog.tsx
+++ src/pages/Vlog.tsx
@@ -15,7 +15,7 @@ const Vlog = () => {
       title: 'Como Desenvolver Líderes de Alta Performance',
       category: 'Liderança',
       description: 'Descubra as 5 competências essenciais que todo líder precisa dominar para alcançar resultados extraordinários.',
-      thumbnail: 'https://www.youtube.com/watch?v=sWp7FYbCnjo&t=25s,
+      thumbnail: 'https://www.youtube.com/watch?v=sWp7FYbCnjo&t=25s',
       duration: '12:34',
       views: '2.5K',
       icon: <Users className="h-6 w-6" />,
